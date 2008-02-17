f2898a4efdf509b186274d222e019273488f8abd
Brian Cully <bjc@kublai.com>
2008-02-17 18:46:16 -0500

    Fix http-bind

--- src/ejabberd_c2s.erl
+++ src/ejabberd_c2s.erl
@@ -1375,14 +1375,14 @@ get_auth_tags([], U, P, D, R) ->
 
 get_conn_type(StateData) ->
     case StateData#state.sockmod of
-    ejabberd_http_poll -> http_poll;
-    ejabberd_http_bind -> http_bind;
-    _ ->
-        case (StateData#state.sockmod):get_sockmod(StateData#state.socket) of
-        ejabberd_zlib -> c2s_compressed;
-        tls -> c2s_tls;
-        gen_tcp -> c2s
-        end
+        ejabberd_http_poll -> http_poll;
+        _ ->
+            case (StateData#state.sockmod):get_sockmod(StateData#state.socket) of
+                ejabberd_http_bind -> http_bind;
+                ejabberd_zlib -> c2s_compressed;
+                tls -> c2s_tls;
+                gen_tcp -> c2s
+            end
     end.
 
 process_presence_probe(From, To, StateData) ->
