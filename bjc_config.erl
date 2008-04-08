f51ff3de0b1df5a4a33c385ec4a384f752c38789
Brian Cully <bjc@kublai.com>
2008-04-08 13:48:58 -0400

    Table creation working

--- bjc_config.erl
+++ bjc_config.erl
@@ -5,9 +5,13 @@
 %-export([start/0, load_file/1]).
 -include("ejabberd.hrl").
 
+-define(TABLE_PREFIX, "config_").
+-record(configuration, {key, value}).
+
 -record(load_info, {includes = sets:new(), terms = [], cwd}).
 
 start() ->
+    ok = debug_start(),
     Config = case application:get_env(config) of
 		 {ok, Path} -> Path;
 		 undefined -> 
@@ -22,7 +26,13 @@ start() ->
 
 load_file(Filename) ->
     {ok, Terms} = preprocess(Filename),
-    ok = process_terms(Terms, default).
+    ok = create_tables(Terms),
+    {atomic, ok} = mnesia:transaction(fun () -> process_terms(Terms, default) end).
+
+debug_start() ->
+    application:start(mnesia),
+    os:putenv("EJABBERD_CONFIG_PATH", "/Users/bjc/src/ejabberd-bjc/test/ejabberd.cfg"),
+    ok.
 
 preprocess(Filename) ->
     Filename2 = filename:absname(Filename),
@@ -56,6 +66,20 @@ find_includes({include, Filename}, #load_info{includes = Includes, cwd = Cwd} = 
 find_includes(Term, #load_info{terms = Other} = Accum) ->
     Accum#load_info{terms = [Term | Other]}.
 
+create_tables(Terms) ->
+    Tables = [default | lists:foldl(fun ({configuration, Table, _Terms}, Accum) ->
+                                            [Table | Accum];
+                                        (_, Accum) -> Accum
+                                    end, [], Terms)],
+    CreateFun = fun (Table) ->
+                        Table2 = list_to_atom(?TABLE_PREFIX ++ atom_to_list(Table)),
+                        mnesia:create_table(Table2,
+                                            [{ram_copies, [node()]},
+                                             {attributes, record_info(fields, configuration)}])
+                end,
+    lists:foreach(CreateFun, Tables),
+    ok.
+
 process_terms(Terms, Table) ->
     {ok, _} = lists:foldl(fun process_terms2/2, {ok, Table}, Terms),
     ok.
@@ -63,8 +87,11 @@ process_terms(Terms, Table) ->
 process_terms2({configuration, Name, Terms}, {ok, Table}) ->
     ok = process_terms(Terms, Name),
     {ok, Table};
-process_terms2(Term, {ok, Table}) ->
-    io:format("DEBUG: ~p -> ~p~n", [Term, Table]),
+process_terms2({Key, Val}, {ok, Table}) ->
+    io:format("DEBUG: ~p:~p -> ~p~n", [Table, Key, Val]),
+    mnesia:write(Table, #configuration{key = Key, value = Val}),
     {ok, Table};
+process_terms2(Term, {ok, _Table}) ->
+    {bad_term, Term};
 process_terms2(_Term, _LastResult) ->
     _LastResult.
